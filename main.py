30584af6ccf09cb9c51ba97dd4e52cf9f508883d
junsoo shin <sjs200201@naver.com>
2024-05-25 15:39:13 +0900

    cors,id

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from todo import todo_router
 import uvicorn
 
 app = FastAPI()
+
+origins =["https://127.0.0.1:5500", "https://52.55.96.33"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(todo_router)
 
@@ -12,4 +23,4 @@ async def welcome() -> dict:
         "msg" : "hello world?"
         }
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
